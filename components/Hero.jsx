2337d3838faae67ab997a7a3f36b8f8e344cfad0
Calathea-Z <zsykes21@gmail.com>
2023-03-30 23:35:06 -0400

    Test

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,13 +1,13 @@
 import Image from "next/image"
 import { heroPic } from "@/public/assets"
-import { SocialIcon } from "react-social-icons"
+// import { SocialIcon } from "react-social-icons"
 
 
 const Hero = () => {
   return (
     <div className='relative w-full flex'>
-      <Image src={heroPic} alt='pots' className='content-fill w-full h-auto' />
-      <div className='opacity-90 bottom-[.2rem] right-[.1rem] z-20 absolute flex gap-[.36rem]'>
+      <Image src={heroPic} alt='pots' priority className='content-fill w-full h-auto' />
+      {/* <div className='opacity-90 bottom-[.2rem] right-[.1rem] z-20 absolute flex gap-[.36rem]'>
       <SocialIcon 
       url='https://www.instagram.com/vineandfrond/'
       fgColor='black'
@@ -20,7 +20,7 @@ const Hero = () => {
       url='https://www.tiktok.com/@vineandfrond'
       fgColor='white'
       />
-      </div>
+      </div> */}
     </div>
   )
 }
