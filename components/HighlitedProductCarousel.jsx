778db38e35117b2e8b4ac4d8a05a11073960ea73
Calathea-Z <zsykes21@gmail.com>
2023-04-06 19:57:27 -0400

    Troubleshooting deploy

--- components/HighlitedProductCarousel.jsx
+++ components/HighlitedProductCarousel.jsx
@@ -1,4 +1,4 @@
-import { ClipLoader } from "react-spinners";
+import ClipLoader from "react-spinners/ClipLoader";
 import { client } from "@/utils/client"
 import { useEffect, useState } from "react";
 import ProductItem from "./ProductItem";
@@ -29,9 +29,9 @@ const HighlightedProductCarousel = () => {
   return (
     <div className="w-full bg-primary flex flex-col justify-center items-center gap-2 p-4">
       <h1 className='text-4xl xl:text-6xl p-4'>This Weeks Favorites</h1>
-      <div className='relative w-full flex overflow-x-scroll overflow-y-hidden scrollbar scrollbar-track-primary scrollbar-thumb-[#caafa8]'>
+      <div className='relative w-full flex overflow-x-scroll overflow-y-hidden scrollbar scrollbar-track-primary scrollbar-thumb-[#caafa8] justify-center'>
       {loading ? <ClipLoader color={"#877570"} className='flex justify-center items-center' /> : error ? "Error please reload" : (products.map((product, index) => (
-        <div key={index} className='flex justify-center rounded-md p-2 flex-shrink-0'>
+        <div key={index} className='flex justify-center rounded-md p-2 flex-shrink-0 w-[200px] lg:w-[260px] xl:w-[360px]'>
           <ProductItem product={product} />
         </div>
       )))}
