b0e93a06f763d02c02b145e39c4f62f80a0a39f1
Calathea-Z <zsykes21@gmail.com>
2023-04-08 15:05:28 -0400

    Working carousel with selector buttons. Adding in photothumbnails next.

--- components/SingleProductCarousel.jsx
+++ components/SingleProductCarousel.jsx
@@ -2,26 +2,42 @@ import { useState } from "react";
 import { urlFor } from "@/utils/image.js";
 import {
   ArrowSmallLeftIcon,
-  ArrowSmallRightIcon
+  ArrowSmallRightIcon,
+  StopIcon,
 } from "@heroicons/react/24/solid";
 
 const SingleProductCarousel = (props) => {
-  const [currentImage, setCurrentImage] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? props.photo.length - 1 : currentIndex -1;
+    setCurrentIndex(newIndex);
+  };
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === props.photo.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
 
   return (
     <div className="max-w-[1400px] h-[500px] md:h-[600px] lg:h-[800px] w-full m-auto py-16 px-4 relative group">
-      {props.photo.map((photo, index) => (
         <div
-        key={index}
-          style={{ backgroundImage: `url(${urlFor(photo.asset._ref).url()})`}}
-          className="w-full h-full rounded-xl bg-center bg-contain bg-no-repeat duration-500"
+          style={{ backgroundImage: `url(${urlFor(props.photo[currentIndex].asset._ref).url()})`}}
+          className="w-full h-full rounded-lg bg-center bg-contain bg-no-repeat duration-500"
         ></div>
-      ))}
+
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/40 text-white cursor-pointer'>
-        <ArrowSmallLeftIcon className='w-10 h-10'/>
+        <ArrowSmallLeftIcon className='w-8 h-8 md:w-10 md:h-10' onClick={prevSlide}/>
       </div>
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/40 text-white cursor-pointer'>
-        <ArrowSmallRightIcon className='w-10 h-10'/>
+        <ArrowSmallRightIcon className='w-8 h-8 md:w-10 md:h-10' onClick={nextSlide}/>
+      </div>
+      <div className='flex left-4 justify-center py-2'>
+        {props.photo.map((thumbnail, index) => (
+            <div key={index} className='cursor-pointer' onClick={() => setCurrentIndex(index)}>
+              <StopIcon className='w-5 h-5'/>
+            </div>
+        ))}
       </div>
     </div>
   );
