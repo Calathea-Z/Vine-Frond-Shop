bffd92a85495c652cb569928bffbd8f77fcda141
Calathea-Z <zsykes21@gmail.com>
2023-04-08 15:15:15 -0400

    Fully functional carousel for individual item. With thumbnail selectors

--- components/SingleProductCarousel.jsx
+++ components/SingleProductCarousel.jsx
@@ -5,6 +5,7 @@ import {
   ArrowSmallRightIcon,
   StopIcon,
 } from "@heroicons/react/24/solid";
+import Image from "next/image";
 
 const SingleProductCarousel = (props) => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -20,7 +21,7 @@ const SingleProductCarousel = (props) => {
   };
 
   return (
-    <div className="max-w-[1400px] h-[500px] md:h-[600px] lg:h-[800px] w-full m-auto py-16 px-4 relative group">
+    <div className="max-w-[1400px] h-[500px] md:h-[600px] lg:h-[700px] w-full m-auto py-16 px-4 relative group">
         <div
           style={{ backgroundImage: `url(${urlFor(props.photo[currentIndex].asset._ref).url()})`}}
           className="w-full h-full rounded-lg bg-center bg-contain bg-no-repeat duration-500"
@@ -32,10 +33,10 @@ const SingleProductCarousel = (props) => {
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/40 text-white cursor-pointer'>
         <ArrowSmallRightIcon className='w-8 h-8 md:w-10 md:h-10' onClick={nextSlide}/>
       </div>
-      <div className='flex left-4 justify-center py-2'>
+      <div className='flex left-4 justify-center py-2 gap-2'>
         {props.photo.map((thumbnail, index) => (
-            <div key={index} className='cursor-pointer' onClick={() => setCurrentIndex(index)}>
-              <StopIcon className='w-5 h-5'/>
+            <div key={index} className='cursor-pointer rounded-sm w-[3rem] h-[3rem] md:w-[5rem] md:h-[5rem] relative' onClick={() => setCurrentIndex(index)}>
+              <Image src={urlFor(thumbnail.asset._ref).url()} fill />
             </div>
         ))}
       </div>
