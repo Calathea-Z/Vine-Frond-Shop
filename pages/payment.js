ba90bbf4ed88790122b939e28d9d64157edb08b3
Calathea-Z <zsykes21@gmail.com>
2023-04-28 09:23:40 -0400

    formatting changes payment screen.

--- pages/payment.js
+++ pages/payment.js
@@ -41,44 +41,10 @@ const PaymentScreen = () => {
 
   const placeOrderHandler = async () => {
     if (userInfo) {
-      try {
-        const { data } = await axios.post(
-          "/api/orders",
-          {
-            orderItems: cartItems.map((x) => ({
-              ...x,
-              countInStock: undefined,
-              slug: undefined,
-            })),
-            shippingInformation: shippingInformation,
-            itemsPrice: itemsPrice,
-            parsedShippingCost: parsedShippingCost,
-            taxPrice: taxPrice,
-            totalPrice: totalPrice,
-          },
-          {
-            headers: {
-              authorization: `Bearer ${userInfo.token}`,
-            },
-          }
-        );
-        await axios.put("/api/allproducts/updatequantity", {
-          cartItems: cartItems,
-        });
-        dispatch({ type: "CART_CLEAR_ITEMS" });
-        jsCookie.remove("cartItems");
-        dispatch({ type: "CLEAR_PAYMENT_STATUS" });
-        jsCookie.remove("orderSuccess");
-
-        router.push(`/order/${data}`);
-      } catch (err) {
-        dispatch({ type: "CLEAR_PAYMENT_STATUS" });
-        jsCookie.remove("orderSuccess");
-        enqueueSnackbar(getError(err), { variant: "error" });
-      }
-    } else {
-      try {
-        const { data } = await axios.post("/api/orders", {  // eslint-disable-line no-unused-vars
+    try {
+      const { data } = await axios.post(
+        "/api/orders",
+        {
           orderItems: cartItems.map((x) => ({
             ...x,
             countInStock: undefined,
@@ -89,30 +55,64 @@ const PaymentScreen = () => {
           parsedShippingCost: parsedShippingCost,
           taxPrice: taxPrice,
           totalPrice: totalPrice,
-        });
-        await axios.put("/api/allproducts/updatequantity", {
-          cartItems: cartItems,
-        });
-        dispatch({ type: "CART_CLEAR_ITEMS" });
-        jsCookie.remove("cartItems");
-        dispatch({ type: "CLEAR_PAYMENT_STATUS" });
-        jsCookie.remove("orderSuccess");
-        // router.push(`/order/${data}`);
-      } catch (err) {
-        enqueueSnackbar(getError(err), { variant: "error" });
-      }
+        },
+        {
+          headers: {
+            authorization: `Bearer ${userInfo.token}`,
+          },
+        }
+      );
+      await axios.put("/api/allproducts/updatequantity", {
+        cartItems: cartItems,
+      });
+
+      dispatch({ type: "CART_CLEAR_ITEMS" });
+      jsCookie.remove("cartItems");
+      dispatch({ type: "CLEAR_PAYMENT_STATUS" });
+      jsCookie.remove("orderSuccess");
+      router.push(`/order/${data}`);
+    } catch (err) {
+      dispatch({ type: "CLEAR_PAYMENT_STATUS" });
+      jsCookie.remove("orderSuccess");
+      enqueueSnackbar(getError(err), { variant: "error" });
     }
+  } else {
+    try {
+      const { data } = await axios.post("/api/orders", {  // eslint-disable-line no-unused-vars
+        orderItems: cartItems.map((x) => ({
+          ...x,
+          countInStock: undefined,
+          slug: undefined,
+        })),
+        shippingInformation: shippingInformation,
+        itemsPrice: itemsPrice,
+        parsedShippingCost: parsedShippingCost,
+        taxPrice: taxPrice,
+        totalPrice: totalPrice,
+      });
+      await axios.put("/api/allproducts/updatequantity", {
+        cartItems: cartItems,
+      });
+      dispatch({ type: "CART_CLEAR_ITEMS" });
+      jsCookie.remove("cartItems");
+      dispatch({ type: "CLEAR_PAYMENT_STATUS" });
+      jsCookie.remove("orderSuccess");
+      // router.push(`/order/${data}`);
+    } catch (err) {
+      enqueueSnackbar(getError(err), { variant: "error" });
+    }
+  }
   };
 
+  // This effect sets isLoading to false when cartItems changes
   useEffect(() => {
     setIsLoading(false);
   }, [cartItems]);
 
+  // This effect calls placeOrderHandler() when orderSuccess changes
   useEffect(() => {
     if (orderSuccess) {
       placeOrderHandler();
-    } else {
-      return;
     }
   }, [orderSuccess]);
 
@@ -120,7 +120,9 @@ const PaymentScreen = () => {
     <div className="p-4">
       <CheckoutHelper activeStep={3} />
       <div className="p-6 flex flex-col justify-center items-center">
-        <h1 className="font-sans underline underline-offset-4 mb-4">Finalize Your Order</h1>
+        <h1 className="font-sans underline underline-offset-4 mb-4">
+          Finalize Your Order
+        </h1>
 
         {isLoading ? (
           <div className="flex justify-center items-center mx-auto h-screen w-full">
@@ -128,15 +130,20 @@ const PaymentScreen = () => {
           </div>
         ) : cartItems.length === 0 ? (
           <div className="flex justify-center flex-col items-center h-screen mt-[-6rem]">
-           <h1>weird.. there is nothing in your cart...</h1>
-            <Link href="/allproducts" className=' flex flex-col justify-center items-center'>
+            <h1>weird.. there is nothing in your cart...</h1>
+            <Link
+              href="/allproducts"
+              className=" flex flex-col justify-center items-center"
+            >
               <Image
                 src={sadCart}
                 height={300}
                 width={300}
                 alt="sad cart empty"
               />
-             <p className='font-sans border-2 rounded-md p-1 bg-primary border-gray-200 shadow-md hover:opacity-80'>Go Shopping.</p>
+              <p className="font-sans border-2 rounded-md p-1 bg-primary border-gray-200 shadow-md hover:opacity-80">
+                Go Shopping.
+              </p>
             </Link>
           </div>
         ) : (
@@ -169,33 +176,41 @@ const PaymentScreen = () => {
                 </div>
               </div>
 
-
               <div className="hidden rounded-lg border-2 shadow-md overflow-x-auto border-gray-300 sm:block p-4 sm:p-5 mb-5">
                 <h2 className="font-sans">Items</h2>
                 <table className="table-auto">
                   <thead className="border-b">
                     <tr>
-                      <th className="px-5  text-left font-sans text-xs sm:text-lg w-full">Item</th>
-                      <th className="p-5 text-right font-sans text-xs sm:text-lg">Quantity</th>
-                      <th className="p-5 text-right font-sans text-xs sm:text-lg">Price</th>
-                      <th className="p-5 text-right font-sans text-xs sm:text-lg ">Subtotal</th>
+                      <th className="px-5  text-left font-sans text-xs sm:text-lg w-full">
+                        Item
+                      </th>
+                      <th className="p-5 text-right font-sans text-xs sm:text-lg">
+                        Quantity
+                      </th>
+                      <th className="p-5 text-right font-sans text-xs sm:text-lg">
+                        Price
+                      </th>
+                      <th className="p-5 text-right font-sans text-xs sm:text-lg ">
+                        Subtotal
+                      </th>
                     </tr>
                   </thead>
                   <tbody>
                     {cartItems.map((item, index) => (
                       <tr key={index} className="border-b">
                         <td>
-                        <Link
-          href={`/allproducts/${item.slug}`}
-          className="flex items-center flex-col">
-          <Image
-            src={urlFor(item.photo[0].asset._ref).url()}
-            alt={item.name}
-            width={80}
-            height={80}
-            class="rounded-md p-1"
-          />
-          <p className='font-sans text-xs text-center'>{item.name}</p>
+                          <Link
+                            href={`/allproducts/${item.slug}`}
+                            className="flex flex-col"
+                          >
+                            <Image
+                              src={urlFor(item.photo[0].asset._ref).url()}
+                              alt={item.name}
+                              width={80}
+                              height={80}
+                              class="rounded-md p-1"
+                            />
+                            <p className="font-sans text-xs">{item.name}</p>
                             &nbsp;
                           </Link>
                         </td>
@@ -213,9 +228,6 @@ const PaymentScreen = () => {
                   </tbody>
                 </table>
                 <div>
-                  
-
-                  
                   <Link
                     href="/cart"
                     className="font-sans text-xs sm:text-sm underline hover:text-blue-400"
@@ -225,51 +237,53 @@ const PaymentScreen = () => {
                 </div>
               </div>
 
-
               <div class="sm:hidden rounded-lg border-2 shadow-md overflow-x-auto border-gray-300 block p-4 sm:p-5 mb-5">
-  <h2 class="font-sans">Items</h2>
-  <div class="flex flex-wrap flex-col mb-5">
-    {cartItems.map((item, index) => (
-      <div key={index} class="flex flex-grow items-center">
-        <Link
-          href={`/allproducts/${item.slug}`}
-          className="flex items-center flex-col">
-          <Image
-            src={urlFor(item.photo[0].asset._ref).url()}
-            alt={item.name}
-            width={80}
-            height={80}
-            class="rounded-md p-1"
-          />
-          <p className='font-sans text-xs text-center'>{item.name}</p>
-          &nbsp;
-        </Link>
-        <div class="w-1/3 text-center flex flex-col">
-          <p className='font-sans text-xs'>Quantity</p>
-          <span class="font-sans">{item.quantity}</span>
-        </div>
-        <div class="w-1/3 text-center flex flex-col">
-          <p className='font-sans text-xs'>Price</p>
-          <span class="font-sans">${item.price}</span>
-        </div>
-        <div class="w-1/3 text-center flex flex-col">
-          <p className='font-sans text-xs'>Subtotal</p>
-          <span class="font-sans">${item.quantity * item.price}</span>
-        </div>
-      </div>
-    ))}
-  </div>
-  <div>
-    <Link
-      href="/cart"
-      class="font-sans text-xs sm:text-sm underline hover:text-blue-400"
-    >
-      Edit
-    </Link>
-  </div>
-</div>
-
-              
+                <h2 class="font-sans">Items</h2>
+                <div class="flex flex-wrap flex-col mb-5">
+                  {cartItems.map((item, index) => (
+                    <div key={index} class="flex flex-grow items-center">
+                      <Link
+                        href={`/allproducts/${item.slug}`}
+                        className="flex items-center flex-col"
+                      >
+                        <Image
+                          src={urlFor(item.photo[0].asset._ref).url()}
+                          alt={item.name}
+                          width={80}
+                          height={80}
+                          class="rounded-md p-1"
+                        />
+                        <p className="font-sans text-xs text-center">
+                          {item.name}
+                        </p>
+                        &nbsp;
+                      </Link>
+                      <div class="w-1/3 text-center flex flex-col">
+                        <p className="font-sans text-xs">Quantity</p>
+                        <span class="font-sans">{item.quantity}</span>
+                      </div>
+                      <div class="w-1/3 text-center flex flex-col">
+                        <p className="font-sans text-xs">Price</p>
+                        <span class="font-sans">${item.price}</span>
+                      </div>
+                      <div class="w-1/3 text-center flex flex-col">
+                        <p className="font-sans text-xs">Subtotal</p>
+                        <span class="font-sans">
+                          ${item.quantity * item.price}
+                        </span>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+                <div>
+                  <Link
+                    href="/cart"
+                    class="font-sans text-xs sm:text-sm underline hover:text-blue-400"
+                  >
+                    Edit
+                  </Link>
+                </div>
+              </div>
             </div>
             <div className="lg:col-start-1"></div>
             <div className="rounded-lg border-2 shadow-md border-gray-300 mb-5 p-6 mx-auto lg:col-start-4 lg:row-start-1">
