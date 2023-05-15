0ae84c2eda3d26b462d8a6480bfe75170de202f2
Calathea-Z <zsykes21@gmail.com>
2023-05-15 09:33:45 -0400

    Begin work on pofile screen.

--- pages/user/profile.js
+++ pages/user/profile.js
@@ -5,13 +5,13 @@ const ProfileScreen = () => {
   return (
     <div>
       <Header />
-      <div className='p-4 flex justify-between items-center'>
+      <div className='p-4 flex justify-between items-start h-screen'>
         <h1 className='py-6 px-8 text-4xl'>My Account</h1>
-        <button className='py-6 px-8 text-3xl'>Log Out</button>
+        <button className='py-3 px-8 text-3xl rounded-md border-2 bg-gray-300 hover:opacity-80'>Log Out</button>
       </div>
       <div className="hidden sm:flex w-[90%] border-gray-300 border-[1px] justify-center items-center mx-auto" />
       <div>
-        
+
       </div>
       <Footer />
     </div>
