40020bc4062aa7a7a6024783707a3d180bbf8b1b
Calathea-Z <zsykes21@gmail.com>
2023-06-01 15:06:23 -0400

    Working on profile page.

--- pages/user/profile.js
+++ pages/user/profile.js
@@ -5,13 +5,22 @@ const ProfileScreen = () => {
   return (
     <div>
       <Header />
-      <div className='p-4 flex justify-between items-start h-screen'>
-        <h1 className='py-6 px-8 text-4xl'>My Account</h1>
+      <div className='p-6 flex justify-start items-start flex-col h-screen'>
+      <div className='p-4 flex justify-between w-screen'>
+        <h1 className='py-5 px-8 text-4xl'>My Account</h1>
         <button className='py-3 px-8 text-3xl rounded-md border-2 bg-gray-300 hover:opacity-80'>Log Out</button>
       </div>
-      <div className="hidden sm:flex w-[90%] border-gray-300 border-[1px] justify-center items-center mx-auto" />
       <div>
-
+      <div>
+          <h2>Name</h2>
+          <h6>name here</h6>
+        </div>
+        <div>
+          <h2>Shipping Address</h2>
+          <h6>Shipping Address here</h6>
+          <button className='py-2 px-4 text-lg rounded-md border-2 bg-gray-300 hover:opacity-80'>Update Shipping Address</button>
+        </div>
+      </div>
       </div>
       <Footer />
     </div>
